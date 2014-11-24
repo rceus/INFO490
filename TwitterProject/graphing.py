8b79961b213c051db655baf0483282357aba6942
rishabhjain <rishabhjain2795@gmail.com>
2014-11-23 22:44:44 -0500

    Twitter Complete

--- TwitterProject/graphing.py
+++ TwitterProject/graphing.py
@@ -1,8 +1,78 @@
+#####################################################################################
+############################### Second Part #########################################
+#####################################################################################
+
+
 import csv
 import numpy as np
 import pandas as pd
 
 cr = csv.reader(open("test.csv","rb"))
 
-for row in cr
-	#do something
+mon = 0
+tue = 0
+wed = 0
+thu = 0
+fri = 0
+sat = 0
+sun = 0
+for row in cr:
+	if row[0].lower() == "mon":
+		mon = mon + 1
+	if row[0].lower() == "tue":
+		tue = tue + 1
+	if row[0].lower() == "wed":
+		wed = wed + 1
+	if row[0].lower() == "thu":
+		thu = thu + 1
+	if row[0].lower() == "fri":
+		fri = fri + 1
+	if row[0].lower() == "sat":
+		sat = sat + 1
+	if row[0].lower() == "sun":
+		sun = sun + 1
+
+# Testing Purposes
+# print mon
+# print tue
+# print wed
+# print thu
+# print fri
+# print sat
+# print sun
+
+#####################################################################################
+############################### Third Part ##########################################
+#####################################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+n_groups = 7
+
+tweets = (mon, tue, wed, thu, fri, sat, sun)
+
+fig, ax = plt.subplots()
+
+index = np.arange(n_groups)
+bar_width = 0.5
+
+opacity = 1
+error_config = {'ecolor': '0.3'}
+
+rects1 = plt.bar(index, tweets, bar_width,
+                 alpha=opacity,
+                 color='b',
+                 error_kw=error_config,
+                 label='UIUC')
+
+
+plt.xlabel('Day of the Week')
+plt.ylabel('Number of Tweets')
+plt.title('Tweets having the term UIUC in them')
+plt.xticks(index + bar_width, ('Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'))
+plt.legend()
+
+plt.tight_layout()
+plt.show()
--- TwitterProject/twittersearch.py
+++ TwitterProject/twittersearch.py
@@ -1,10 +1,15 @@
+#####################################################################################
+############################### First Part #########################################
+#####################################################################################
+
+
 import datetime
 import csv
 from TwitterSearch import *
 try:
-    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['uiuc']) # let's define all words we would like to have a look for
-    tso.set_language('en') # we want to see German tweets only
+    tso = TwitterSearchOrder()      # create a TwitterSearchOrder object
+    tso.set_keywords(['uiuc'])      # let's define all words we would like to have a look for
+    tso.set_language('en')          # we want to see English tweets only
     tso.set_include_entities(False) # and don't give us all those entity information
     #tso.set_since(datetime.date(2014, 11, 8))
     #tso.set_count(10)
@@ -20,20 +25,16 @@ try:
 
     # this is where the fun actually starts :)
     number = 0
-    c = csv.writer(open("test.csv", "wb"))
+    c = csv.writer(open("test2.csv", "wb"))
 
     for tweet in ts.search_tweets_iterable(tso):
         print( '%s : %s : %s' % ( tweet['user']['screen_name'], tweet['created_at'], tweet['retweet_count']) )
         ts =  tweet['created_at']
-        c.writerow([ts[:3], tweet['retweet_count']])
+        c.writerow([ts[:3].lower(), tweet['retweet_count']])
         number = number+1
 
     print "Total Number of Links:", number
 
-    #part1: order by retweet count
-    #part2: make a csv file of it: just timestamps (done)
-    #mid23: add tweets per day and show them 
-    #part3: take the data from the csv file to make a scatter plot
 
 except TwitterSearchException as e: # take care of all those ugly errors if there are some
     print(e)
